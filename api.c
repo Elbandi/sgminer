4263f317b059897f5fc4c2279048e8e4100e8d9d
Elbandi <elso.andras@gmail.com>
2014-04-26 00:46:01 +0200

    Protect pool name with lock

--- api.c
+++ api.c
@@ -2070,7 +2070,9 @@ static void poolstatus(struct io_data *io_data, __maybe_unused SOCKETTYPE c, __m
 			lp = (char *)NO;
 
 		root = api_add_int(root, "POOL", &i, false);
-		root = api_add_string(root, "Name", get_pool_name(pool), false);
+		mutex_lock(&pool->stratum_lock);
+		root = api_add_string(root, "Name", get_pool_name(pool), true);
+		mutex_unlock(&pool->stratum_lock);
 		root = api_add_escape(root, "URL", pool->rpc_url, false);
 		root = api_add_string(root, "Algorithm", (char *)pool->algorithm.name, false);
 		root = api_add_string(root, "Description", pool->description, false);
